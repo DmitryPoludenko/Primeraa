979f0db1fa1d11702579b6ade3bd2869808e28a5
poludas123 <poludas123@gmail.com>
2019-10-17 18:37:08 +0300

    commit

--- Primera/Program.cs
+++ Primera/Program.cs
@@ -7,6 +7,7 @@ namespace Primera
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.ReadLine();
         }
 
         public void Primer()
