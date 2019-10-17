91fe9942cb6a4936279769868b8916fe8e1ad1d6
poludas123 <poludas123@gmail.com>
2019-10-17 16:08:26 +0300

    Second commit

---
+++ Primera/Evgeniy.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Primera
+{
+    class Evgeniy : Interface1
+    {
+        public void Primer()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
